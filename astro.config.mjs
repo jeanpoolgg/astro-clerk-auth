906ceaa0c1ddeaa1fc42a668080c8e06f2fa090c
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-09-13 16:06:25 -0500

    feat(auth): add and configure Clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [clerk()],
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  adapter: node({ mode: "standalone" }),
+  output: "server",
 });

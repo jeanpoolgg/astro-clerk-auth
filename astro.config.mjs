ee567244a847470f85747c48ac997de4789e2c65
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-09-13 16:43:28 -0500

    feat(auth): configure Clerk localization to Spanish

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import clerk from "@clerk/astro";
 import tailwindcss from '@tailwindcss/vite';
+import { esES } from '@clerk/localizations';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [clerk()],
+  integrations: [clerk({
+    localization: esES
+  })],
   vite: {
     plugins: [tailwindcss()]
   },
